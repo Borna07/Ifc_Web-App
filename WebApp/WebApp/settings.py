0322373c3cdbce8dc24994797bbc366e39163917
DESKTOP-E8FCUEG\Borna <borna.molnar-erhatic@gmx.at>
2022-01-29 11:01:09 +0100

    123

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -120,6 +120,10 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
