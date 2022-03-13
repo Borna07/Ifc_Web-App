7a774ee569d400bf2faf21ee3f7dbb243ea0441d
Borna07 <borna.molnar-erhatic@gmx.at>
2022-03-13 10:16:13 +0100

    STATIC_ROOT

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -118,6 +118,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
